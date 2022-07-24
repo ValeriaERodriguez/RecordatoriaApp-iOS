b368eb0dc064afc5fc1e1ebf5c5ccaa67954cb41
Valeria Rodriguez <valeriaelisabetrgz@gmail.com>
2022-07-24 20:10:34 -0300

    Initial Commit

---
+++ Recordatorio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recordatorio
+//
+//  Created by Mac on 24/07/2022.
+//  Copyright © 2022 Valeria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
